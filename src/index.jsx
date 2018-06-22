3da2e163111aaa0ee5c74a791ea69c2381176ffd
julia sheremet <juliajessicasher@gmail.com>
2018-06-22 15:52:28 -0700

    add webpakc, babel and transpile JSX to js on the dom to test dependencies

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Thadeus and Emma</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
+
+// function liveTime(){
+// let clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+
+// }
+
+// setInterval(liveTime, 1000);

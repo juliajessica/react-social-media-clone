6471ea85e2c73b50934a49324e0ea2e93b0bfc13
julia sheremet <juliajessicasher@gmail.com>
2018-06-22 16:29:16 -0700

    create an app component and render it to the dom

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App(){
+  return (
+    <div>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>Thadeus and Emma</h3>
+      <p><em>Firebase will not save record!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
+
+
+// function liveTime(){
+// let clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+
+// }
+
+// setInterval(liveTime, 1000);
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3a</h3>
-    <h3>Thadeus and Emma</h3>
-    <p><em>Firebase will not save record!</em></p>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
-
-// function liveTime(){
-// let clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-
-// }
-
-// setInterval(liveTime, 1000);

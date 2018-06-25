8295a2de401e1dde2ff122983ebacc0fc7303ce7
julia sheremet <juliajessicasher@gmail.com>
2018-06-25 11:11:07 -0700

    add esliter to webpack bundle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
 function Header(){
+  let headerStyles = {
+    fontWeight: 'bold',
+    fontFamily: 'monospace',
+    fontSize: '28px'
+  };
+
   return(
-    <h1>Help Queue</h1>
+    <div style={headerStyles} className="green-text">
+      <h1>Help Queue</h1>
+
+      <style jsx>{`
+        .green-text {
+          color: green;
+        }
+        .green-text:hover {
+          background-color: pink;
+          opacity: 0.5;
+        }
+      `}</style>
+    </div>
+
   );
 }
 
